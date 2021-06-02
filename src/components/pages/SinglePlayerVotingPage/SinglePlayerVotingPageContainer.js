dd4815f5a1a1935c77376357adb20328a7e100e1
Matthew Martin <matthew-martin@lambdastudents.com>
2021-06-01 20:11:30 -0500

    Configued the SinglePlayerVotingPageContainer

--- src/components/pages/SinglePlayerVotingPage/SinglePlayerVotingPageContainer.js
+++ src/components/pages/SinglePlayerVotingPage/SinglePlayerVotingPageContainer.js
@@ -3,8 +3,21 @@ import { useOktaAuth } from '@okta/okta-react';
 import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import renderSinglePlayerVotingPage from './renderSinglePlayerVotingPage';
+import RenderSinglePlayerVotingPage from './RenderSinglePlayerVotingPage';
 
 const SinglePlayerVotingPageContainer = props => {
-  return <div></div>;
+  const { push } = useHistory();
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
+  // eslint-disable-next-line
+  const [memoAuthService] = useMemo(() => [authService], []);
+  const [votes, setVotes] = useState();
+
+  return (
+    <div>
+      <RenderSinglePlayerVotingPage />
+    </div>
+  );
 };
+
+export default SinglePlayerVotingPageContainer;
